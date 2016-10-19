aa7ea8623463c2db579398aa50a40d85a4695ed9
Christopher Lamlamay <Christopher@Christophers-MacBook-Pro.local>
2016-10-19 12:26:13 -0500

    added users indvidually

--- controllers/user.js
+++ controllers/user.js
@@ -25,4 +25,19 @@ ctrl.get('/', function(req, res, next){
 });
 
 
+ctrl.get('/1', function(req, res, next){
+	res.json(users[0]);
+});
+
+
+ctrl.get('/2', function(req, res, next){
+	res.json(users[1]);
+});
+
+ctrl.get('/3', function(req, res, next){
+	res.json(users[2]);
+});
+
+
+
 module.exports = ctrl;
